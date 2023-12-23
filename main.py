371867e7badd27c02ab23ffc4850d6da7c5f5e5b
Divij Mishra <divijmishra@gmail.com>
2023-12-23 15:12:57 -0500

    Added matchup messages.

--- main.py
+++ main.py
@@ -15,6 +15,34 @@ def game_func(action: str) -> bool:
         print("I see that you have accepted defeat!")
         return False
 
+    action_dict = {
+        # contains all possible action pairs, sorted.
+        ("lizard", "lizard"): "The lizards are staring at each other lizardly.",
+        ("lizard", "paper"): "The lizard devoured the paper!",
+        ("lizard", "rock"): "This is how the dinosaurs went extinct.",
+        ("lizard", "scissors"): "The scissors executed the lizard.",
+        ("lizard", "Spock"): "The lizard was venomous and it bit Spock. Spock died.",
+        ("paper", "paper"): "The papers combined and became a birthday card.",
+        (
+            "paper",
+            "rock",
+        ): "The paper covered the rock. The rock doesn't like being covered, apparently.",
+        ("paper", "scissors"): "The paper is now two papers.",
+        (
+            "paper",
+            "Spock",
+        ): "The paper disproved Spock and gave him a paper cut. Spock is crying uncontrollably in the corner.",
+        ("rock", "rock"): "We're stuck between a rock and a hard place.",
+        (
+            "rock",
+            "scissors",
+        ): "The scissors smashed themselves against the unbreakable rock.",
+        ("rock", "Spock"): "Spock vaporized the rock. Seemed excessive.",
+        ("scissors", "scissors"): "The scissors accepted an honorable draw.",
+        ("scissors", "Spock"): "Spock destroyed the scissors because he could.",
+        ("Spock", "Spock"): "Spock stopped looking at himself in the mirror.",
+    }
+
     print("Testing.")
     return True
 
